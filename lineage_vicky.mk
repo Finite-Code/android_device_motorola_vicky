09de33074163f68af87cf7bc09522540b5474d09
Atharva Pandey <93546392+Finite-Code@users.noreply.github.com>
2026-02-01 00:52:00 +0530

    Modify copyright and product details in lineage_vicky.mk
    Updated copyright year and changed product name to LineageOS.

--- lineage_vicky.mk
+++ lineage_vicky.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2025 The Android Open Source Project
+# Copyright (C) 2026 The LineageOS Open Source Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -8,27 +8,25 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Evolution X stuff.
-$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
+# Inherit some common LineageOS stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Inherit from vicky device.
 $(call inherit-product, device/motorola/vicky/device.mk)
 
-# EvolutionX flags
-EVO_BUILD_TYPE := Unofficial
-TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_BUILD_APERTURE_CAMERA := false
-TARGET_HAS_UDFPS := true
-TARGET_SUPPORTS_QUICK_TAP := true
-PERF_ANIM_OVERRIDE := true
-
-## Device identifier
-PRODUCT_BRAND := motorola
+# Device identifier. This must match your lunch combo
 PRODUCT_DEVICE := vicky
+PRODUCT_NAME := lineage_vicky
+PRODUCT_BRAND := motorola
+PRODUCT_MODEL := moto g54 5G
 PRODUCT_MANUFACTURER := motorola
-PRODUCT_NAME := evolution_vicky
 
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="vicky_g_sys-user 13 T2SVS33.68-21-6-13 9ff85-633df5 release-keys"
+
+# LineageOS specific properties
+BUILD_FINGERPRINT := motorola/vicky_g_sys-user 13 T2SVS33.68-21-6-13 9ff85-633df5 release-keys
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.build.fingerprint=$(BUILD_FINGERPRINT)
